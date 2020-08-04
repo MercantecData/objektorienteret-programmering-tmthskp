80c9657cff719875844c1803ea6f822d45575028
Tommy <tmth.skp@edu.mercantec.dk>
2020-08-04 13:21:37 +0200

    Tilføjet mine øvelser til dictionary 1 og 2 opg

--- Dictinary_opg_1/Dictinary_opg_1/Program.cs
+++ Dictinary_opg_1/Dictinary_opg_1/Program.cs
@@ -5,7 +5,8 @@ class Program
 {
     static void Main()
     {
-        // initialize data.
+      
+        // måde hvorpå man kan tilføje data til et dictionary
         Dictionary<string, int> data = new Dictionary<string, int>()
         {
             {"Superman", 1},
@@ -20,6 +21,7 @@ class Program
             {"Robocop", 10}
         };
 
+        // Tests af sammensætninger til visning
         // Store keys in a List.
         var list = new List<string>(data.Keys);
         // Loop through the List.
@@ -35,10 +37,54 @@ class Program
              Console.WriteLine("FOREACH KEYVALUEPAIR: {0}, {1}", pair.Key, pair.Value);
          }
 
-        // Use var keyword to enumerate Dictionary.
-        /* foreach (var pair in data)
+        // Use var keyword to view Dictionary.
+         foreach (var pair in data)
          {
              Console.WriteLine("FOREACH VAR: {0}, {1}", pair.Key, pair.Value);
-         }*/
+         }
+
+
+        // Gennemgang af dictionary i klassen anden måde at gøre det på
+        var ageOfStudents = new Dictionary<string, int>();
+        
+        ageOfStudents["Tommy"] = 37;
+        ageOfStudents["Mads"] = 29;
+        ageOfStudents["Bob"] = 27;
+        ageOfStudents["Flop"] = 20;
+        ageOfStudents["Sam"] = 17;
+        ageOfStudents["Clam Jr"] = 19;
+        ageOfStudents["Clam"] = 42;
+        ageOfStudents["Clop"] = 20;
+        ageOfStudents["Bloat"] = 17;
+        ageOfStudents["Dex"] = 19;
+
+        // Loop over pairs with foreach.
+        foreach (KeyValuePair<string, int> pair in ageOfStudents)
+        {
+            Console.WriteLine("FOREACH KEYVALUEPAIR: {0}, {1}", pair.Key, pair.Value);
+        }
+
+        Console.WriteLine(ageOfStudents[Console.ReadLine()]);
+
+       
+        // booleans with float as keys
+        var booleanFloats = new Dictionary<float, bool>();
+
+        booleanFloats[1f] = true;
+        booleanFloats[2f] = false;
+        booleanFloats[3f] = true;
+        booleanFloats[4f] = false;
+        booleanFloats[5f] = true;
+        booleanFloats[6f] = false;
+        booleanFloats[7f] = true;
+        booleanFloats[8f] = false;
+        booleanFloats[9f] = true;
+        booleanFloats[10f] = false;
+
+        // Use var keyword to enumerate Dictionary.
+        foreach (var pair in booleanFloats)
+        {
+            Console.WriteLine("FOREACH VAR: {0}, {1}", pair.Key, pair.Value);
+        }
     }
 }
